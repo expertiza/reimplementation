186a375ae0080032f3a30074456876fa8aaaf838
achitti2812 <achitti@ncsu.edu>
2024-03-23 22:27:10 -0400

    added code to badges route and badges_spec and swagger for API Doc

--- app/controllers/api/v1/badges_controller.rb
+++ app/controllers/api/v1/badges_controller.rb
@@ -1,16 +1,22 @@
 class Api::V1::BadgesController < ApplicationController
-  before_action :set_badge, only: %i[ show update destroy ]
+  before_action :set_badge, only: [:show, :edit, :update, :destroy]
+  before_action :set_return_to, only: [:new]
 
   # GET /badges
   def index
     @badges = Badge.all
 
-    render json: @badges
+    render json: { badges: @badges }, status: :ok
+  end
+
+  def new
+    @badge = Badge.new
+    render json: { badge: @badge }, status: :ok
   end
 
   # GET /badges/1
   def show
-    render json: @badge
+    render json: { badge: @badge }, status: :ok
   end
 
   # POST /badges
@@ -18,24 +24,25 @@ class Api::V1::BadgesController < ApplicationController
     @badge = Badge.new(badge_params)
 
     if @badge.save
-      render json: @badge, status: :created, location: @badge
+      render json: { badge: @badge }, status: :created
     else
-      render json: @badge.errors, status: :unprocessable_entity
+      render json: { errors: @badge.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /badges/1
   def update
     if @badge.update(badge_params)
-      render json: @badge
+      render json: { badge: @badge }, status: :ok
     else
-      render json: @badge.errors, status: :unprocessable_entity
+      render json: { errors: @badge.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   # DELETE /badges/1
   def destroy
     @badge.destroy
+    render json: { message: "Badge was successfully destroyed." }, status: :ok
   end
 
   private
@@ -44,8 +51,12 @@ class Api::V1::BadgesController < ApplicationController
       @badge = Badge.find(params[:id])
     end
 
+    def set_return_to
+      session[:return_to] ||= request.referer
+    end
+
     # Only allow a list of trusted parameters through.
     def badge_params
-      params.require(:badge).permit(:name, :description, :image_name)
+      params.require(:badge).permit(:name, :description, :image_name, :image_file)
     end
 end
